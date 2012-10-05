96f6643cea11f5387c5bf2c51de3bec3d070dd13
Violet Peña <vpena@oberlin.edu>
2012-10-05 16:29:33 -0400

    About to use the script that Harris made originally and see how it works,

--- leaf.js
+++ leaf.js
@@ -1,10 +1,11 @@
-var map = L.map('map', {
-	center: [18, -28],
-	zoom: 5,
-	
+var tiles = new L.tileLayer( 'tiles/{x}_{y}.jpg', {
+	attribution: '&copy; Kevin Reeves 2012',
+	maxZoom: 5,
 });
 
-L.tileLayer( 'tiles/{x}_{y}.jpg', {
-	attribution: '&copy; Kevin Reeves 2012',
-    maxZoom: 18,
-}).addTo(map);
+var map = L.map('map', {
+		center: [73, -53],//first var is y; you go lower when number goes lower; second number is x; you go right when abs. val of number is lower
+		zoom: 5,
+		layers: tiles,
+	});
+
