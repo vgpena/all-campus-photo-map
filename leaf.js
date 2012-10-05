29d5316837afe2ea833a40b908008e383ad6c099
Violet Peña <vpena@oberlin.edu>
2012-10-05 10:37:25 -0400

    Removed the array.

--- leaf.js
+++ leaf.js
@@ -4,18 +4,6 @@ var map = L.map('map', {
 	
 });
 
-var x = 0,
-tiles = new Array();
-
-while (x < 26){
-	var y = 0;
-	while (y < 18){
-		tiles.push('tiles/'+x+'_'+y+'.jpg')
-		y++
-	}
-	x++
-}
-
 map.setView([18, -28], 5);
 
 L.tileLayer( 'tiles/{x}_{y}.jpg', {
